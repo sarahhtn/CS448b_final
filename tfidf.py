500b8a342df3ca1ea2f45d3a4524f7c2288950fa
Sarah Nader <snader2@stanford.edu>
2016-05-31 16:18:26 -0700

    first commit

---
+++ tfidf.py
@@ -0,0 +1,15 @@
+from gensim import corpora, models, similarities
+from pprint import pprint
+
+corpus = ["At work","New job","Enjoying","Beer","Days off","wedding","Office","Drinks","Wine","Drinks","Blessed","A drink","Hubby","Much needed","New place","Thankful","apartment","Excited about","Vacation","Celebrate","Let me know","Had a blast","laundry","care of","company","Grocery","Wishes","Drinking for eveveryone","After work","To work tommorow","Bills","taxes","Husband","shift","The bar","Potty","ready to","Celebrating","To enjoy","My babies","Errands","Relaxing","apt","Fingers crossed","Poor baby","Day to all","women","Work","Yard","Doesn't","Uni","Days","Volunteer","Schedule","repeat","House","Apartment","Moving","place","Rent","Move","Month","Bedroom","Lease","Signed","Roommate","Interested","Complex","Area","Interest","apt","Drinking","Beer","Drink","Cold","Root","Beers","Pong","Ale","Ginger","Cans","Drinkin","ginger","Pint","Cans","Bbq","Pub","bottles","Home","Work","Ready","Hubby","Bed","Dinner","relax","Shower","Heading","Relaxing","Chill","Nap","Early","Supper","Snuggle","Money","Pay","Bills","Paid","Paying","Bill","Job","Month","Rent","Check","Taxes","Bucks","Debt","paycheck","job","Position","Company","Interview","Experience","Manager","Assistant","Interested","Career","Business","Resume","Sales","Hiring","Hire"]
+stoplist = set('for a of the and to in'.split())
+
+texts = [[word for word in string.lower().split() if word not in stoplist]
+			for string in corpus]
+
+
+pprint(texts)
+
+dictionary = corpora.Dictionary(texts)
+dictionary.save('/tmp/words.dict') #store dictionary for future reference
+print(dictionary.token2id)
